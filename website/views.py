1ebb06bcab321b773d870d1de18c3eea7b7a2cae
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-19 23:25:49 +0530

    CRM:9 Changed home view to see records

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Records
 
 
 def home(request):
+    records = Records.objects.all()
+
     # check to see if logging in
     # user is trying to fill the form
     if request.method == 'POST':
@@ -21,7 +24,7 @@ def home(request):
             return redirect('home')
 
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
