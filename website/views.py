b0100092b32621bd45f29cef47db0d9e9e81cd78
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-19 16:55:59 +0530

    CRM:6 Added Login

--- website/views.py
+++ website/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    # user is trying to fill the form
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You Have Been Logged In!')
+            return redirect('home')  # view name is passed
+        else:
+            messages.success(request, 'There Was an error Logging In, Please try again...')
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
