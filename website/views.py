383e5a9464389197ad0bb37c278e7841a41f5b25
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-25 02:18:46 +0530

    CRM:14 Update record

--- website/views.py
+++ website/views.py
@@ -86,3 +86,17 @@ def add_record(request):
     else:
         messages.success(request, "You must be logged to add")
         return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Records.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated!")
+            return redirect('home')
+        return render(request, "update_record.html", {"form": form})
+    else:
+        messages.success(request, "You must be logged to update")
+        return redirect('home')
